6e4d8008fafef0ceece7ccc3e5c66ed33bb7eac5
Ed Soanes <ed@schoolcomms.co.uk>
2016-09-19 16:29:16 +0100

    Animal type script update

--- PetGame.Db/Scripts/AddPetTypes.sql
+++ PetGame.Db/Scripts/AddPetTypes.sql
@@ -2,7 +2,7 @@
 VALUES('Cyclops', 100, 20, 10, 1, 50, 0, 0, 1) --Cyclops never gets unhappy
 
 INSERT INTO AnimalType(Name, MaxHunger, HungerDecreasePerFeed, HungerIncreasePerMin, FeedInterval, MaxHappiness, HappinessIncreasePerPet, HappinessDecreasePerMin, PettingInterval)
-VALUES('Automaton', 100, 0, 0, 1, 50, 10, 1, 1) --Cyclops never gets hungry
+VALUES('Automaton', 100, 0, 0, 1, 50, 10, 1, 1) --Automaton never gets hungry
 
 INSERT INTO AnimalType(Name, MaxHunger, HungerDecreasePerFeed, HungerIncreasePerMin, FeedInterval, MaxHappiness, HappinessIncreasePerPet, HappinessDecreasePerMin, PettingInterval)
-VALUES('Werewolf', 100, 20, 10, 1, 50, 10, 1, 1) --Cyclops never gets hungry
+VALUES('Werewolf', 100, 20, 10, 1, 50, 10, 1, 1) --Werewolf needs both
